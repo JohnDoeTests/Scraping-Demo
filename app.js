884ca90bb8e2925ad874e7504d4653900431225f
Ram Levy <ramlevy@gmail.com>
2020-02-11 23:55:06 +0200

    Refactoring with async/await

--- app.js
+++ app.js
@@ -1,72 +1,88 @@
-const puppeteer = require('puppeteer');
-const elementButton = "#signup_step_one > div.step_content > form.col.col_choose.col_newnumbers.sub_form.form_validator > div > fieldset > div.radiobox_col.checkbox.checkbox_init > label > span";
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const readline = require("readline");
+var firstLaunch = true;
+var page;
 
-GetPhoneNumber();
 
-function CheckSpecialNumber(number) {
-  let numberWithoutPrefix = number.substring(3);
-  const set = new Set([]);
-  var digitCount = 0;
-
-  for (let i = 0; i < numberWithoutPrefix.length; i++) {
-    if (!set.has(numberWithoutPrefix[i])) {
-      set.add(numberWithoutPrefix[i])
-      digitCount++;
-    }
-  }
-  console.log(number + ", Unique digit count:" + digitCount);
-
-  // Checks if the generated number has less than 3 unique digits.
-  // Also checks if the number was fetched correctly.
-  if (digitCount < 3 && number.length > 6) {
-    return true
-  }
-  else {
-    return false
-  }
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+StartScraping().catch(err =>
+  console.log("ERRRRRRRRRRRRRRRORRRRRRRRRRRRRRRRR:  " + err)
+);
 
-async function GetPhoneNumber() {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1400, height: 800 })
-  await page.goto('https://019mobile.co.il/%D7%94%D7%A8%D7%A9%D7%9E%D7%AA-%D7%9C%D7%A7%D7%95%D7%97/?pack_id=10035');
-  await page.screenshot({ path: 'google.png' });
-
-  while (true) {
-    await page.click(elementButton);
-    await page.waitFor(300);
-    const result = await page.evaluate(() => {
-      // Target the 3 generated number values
-      var number1 = document.querySelector('#checkbox_2').value;
-      var number2 = document.querySelector('#checkbox_3').value;
-      var number3 = document.querySelector('#checkbox_4').value;
-
-      var numbers = []
-      numbers.push(number1);
-      numbers.push(number2);
-      numbers.push(number3);
-
-      return numbers;
-    });
-
-    var isSpecial = false;
-    var isAnySpecialNumber = false;;
-    for (let i = 0; i < result.length; i++) {
-      isSpecial = CheckSpecialNumber(result[i]);
-      if (isSpecial)
-        isAnySpecialNumber = true;
-    }
-
-    if (isAnySpecialNumber) {
-      console.log("SUCCESS!!!")
-      break;
-    }
-    else {
-      await page.click(elementButton);
-      await page.click(elementButton);
-      console.log("RETRYING...")
-    }
+async function StartScraping() {
+  if (firstLaunch) {
+    firstLaunch = false;
+    const browser = await puppeteer.launch({ headless: false });
+     page = await browser.newPage();
+    console.log("1")
+    await page.setViewport({ width: 1400, height: 800 });
+    console.log("2")
+    await page.goto(
+      "https://www.yad2.co.il/realestate/forsale?city=6300&property=1,3&rooms=2.5--1&price=-1-1900000"
+    );
+    console.log("3")
   }
+  // await page.screenshot({ path: "google.png" });
+
+  console.log("4")
+  while(true){
+    var selected = await page.waitForSelector(".feeditem");
+    console.log(selected)
+    if(selected)
+      break;
+  }
+
+  await page.evaluate(async () => {
+    await sleep(5000)
+    var deals = document.getElementsByClassName("feeditem");
+
+    // #deals.length
+    for (var i = 0; i < deals.length; i++) {
+      console.log("deal number " + i);
+      var dealElement = document.getElementById(deals[i].children[0].id);
+      dealElement.click();
+      await sleep(2000)
+
+      console.log("deal number " + i + " ID");
+      var id = dealElement.children[1].children[0]
+        .getElementsByClassName("footer")[0]
+        .innerText.match(/\d+/)[0];
+        console.log("deal number " + i + " Price");
+      var price = dealElement.children[0]
+        .getElementsByClassName("left_col")[0]
+        .getElementsByClassName("price")[0].innerText;
+      console.log("deal number " + i + " desc");
+      var desc = dealElement.children[0].getElementsByClassName("right_col")[0]
+        .innerText;
+        dealElement.click();
+
+      // var deal = {
+      //   id: id,
+      //   price: price,
+      //   desc: desc
+      // };
+
+      // b_dealAlreadyExists = false;
+      // var dealID = deal.id;
+
+      // // const content = await window.readfile('/etc/hosts');
+
+      // if (!b_dealAlreadyExists && deal) {
+      //   fs.appendFile("deals.txt", deal.id + "\n", function(err) {
+      //     if (err) throw err;
+      //     console.log("New deal added!");
+      //   });
+
+      //   // MAIL HERE on new shit
+      // } else {
+      //   console.log("Deal already exists");
+      // }
+    }
+  });
+console.log("end")
+  // StartScraping();
 }
